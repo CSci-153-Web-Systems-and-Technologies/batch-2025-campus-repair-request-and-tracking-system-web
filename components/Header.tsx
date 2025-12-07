71d2cf2ad70682e983d61a39e98fffc994ce4a93
Angielyn J. Dacutanan <23-1-00917@vsu.edu.ph>
2025-12-08 07:01:47 +0800

    fix(auth): resolve sign out button functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,15 @@
+import { useRouter } from "next/navigation";
+import { createClient } from "@/utils/supabase/client";
     interface HeaderProps {
     userName?: string;
     }
 
     export default function Header({ userName = "Angielyn" }: HeaderProps) {
+        const router = useRouter();
+        const handleSignOut = async () => {
+
+        await createClient().auth.signOut();
+        }
     return (
         <header className="bg-white shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-3 sm:py-4">
